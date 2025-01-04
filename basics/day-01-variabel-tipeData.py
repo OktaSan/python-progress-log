f569e66393ceabcdcca62fb23cb8cf22b4302e70
Okta Putra Ramadhan <oktaxd88@gmail.com>
2025-01-04 08:37:46 +0700

    day 1: variabel and type data

---
+++ basics/day-01-variabel-tipeData.py
@@ -0,0 +1,33 @@
+#terdapat 4 tipe data basic yang ada di python, yaitu:
+
+#string (str)
+#boleh menggunakan petik dua (")
+print("Hello, World!")
+#dan juga boleh meggunakan petik satu (')
+print('Hello, World!')
+
+#integer (int) => bilangan bulat
+print(19)
+
+#float (desimal, menggunakan . sebagai gantinya ,)
+print(3.14)
+
+#boolean
+#True untuk nilai benar (1)
+print(True) 
+#False untuk nilai salah (0)
+print(False)
+
+
+#variabel adalah suatu tempat untuk menyimpan suatu nilai data
+#aturan penamaan yang valid hanya boleh menggunakan {a-z, A-Z, 0-9, _}
+sapa = "selamat pagi semua!"
+PANGGIL = "woiiiii"
+makan_pagi = "nasgor"
+
+#khusus untuk penamaan variabel tidak boleh meletakkan angka didepan
+nama1 = "ryan"
+
+#penamaan yang lebih rapi menggunakan camel-case
+makanMalam = "mie instan"
+makanSiang = "spageti"
